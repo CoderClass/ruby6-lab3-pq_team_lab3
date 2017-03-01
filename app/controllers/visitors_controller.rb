bc93eac9ff5bd69e7622329a3b97c8b111758d05
Ubuntu <ubuntu@rails-dev-box>
2017-03-01 13:42:55 +0000

    Milestone 5

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,9 @@
 class VisitorsController < ApplicationController
   def index
     @products = Product.all
+    if params[:sort]
+    	@products = Product.alphabetical.to_a
+    end
+
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe ".alphabetical" do
+  	it "return [] when there are no products" do
+  		expect(Product.alphabetical.to_a).to eq []
+  	end
+
+  	it "return [a] when there is only one product a" do
+  		a = Product.create!(name:'a')
+  		expect(Product.all).to eq [a]
+  	end
+
+  	it "return [a, b, c] after I create 3 products c, b, a" do
+  		c = Product.create!(name:'c')
+  		b = Product.create!(name:'b')
+  		a = Product.create!(name:'a')
+  		expect(Product.alphabetical.to_a).to eq [a, b, c]
+  	end
+  end
 end
